d6e17de9dcc12405fa839332903ead972f3316f2
paulbriar <pbriar@live.com>
2019-03-12 20:53:17 -0300

    Set up boilerplate

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
+import reducers from './src/reducers';
+import Header from './src/components/Header';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Header headerText="Manager" />
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Header headerText="Manager" />
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </Provider>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   textStyle: {
     color: 'white',
     fontSize: 20,
-  }
+  },
 });
 
 export default Header;
